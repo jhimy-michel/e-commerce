347a32c3499fdcdb82f370a3fa159d96ceb371e7
jhimy-michel <jhimy0riv@gmail.com>
2022-09-27 21:09:31 +0200

    enabling tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './NavBar';
 
-test.skip('renders learn react link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/E-commerce App/i);
   expect(linkElement).toBeInTheDocument();
 });
