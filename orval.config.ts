e3719d98cd34b677aee846e7596e0befffa30ec8
jhimy-michel <jhimy0riv@gmail.com>
2022-09-27 21:33:06 +0200

    integrating orval and adding script generator

---
+++ orval.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  ecommerce: {
+    output: {
+      mode: 'tags',
+      target: 'src/api/endpoints/ecommerceApi.ts',
+      client: 'react-query',
+      override: {
+        /* mutator: {
+          path: "./src/api/mutator/custom-instance.ts",
+          name: "customInstance"
+        }, */
+        query: {
+          useQuery: true,
+          useInfinite: false,
+          useInfiniteQueryParam: 'limit'
+        }
+      }
+    },
+    input: {
+      target: './openapi.json'
+    }
+  }
+});
---
+++ scripts/genBackendTypes.sh
@@ -0,0 +1,17 @@
+# BASH script to generate types
+# based on the openapi.json file found
+# in the e-commerce-api project
+
+cd "$(dirname "$0")"
+cd ..
+
+rm -f openapi.json
+wget localhost:3000/openapi.json
+
+cd src/api
+rm -fR endpoints
+
+cd ..
+cd ..
+
+npx orval
